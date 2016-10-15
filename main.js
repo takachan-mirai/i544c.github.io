3846f5f2659b36738ed457c58962dbc3c66efd28
Isaac <r.sato7690@me.com>
2016-10-15 17:39:46 +0900

    Fix indent

--- main.js
+++ main.js
@@ -3,45 +3,45 @@
  */
 
 $(function(){
-	w = $(window).width();  //ウィンドウ幅
-	h = $(window).height(); //ウィンドウ高さ
-	h1 = $("#top h1");
-	h1w = h1.width();
-	h1h = h1.height();
+  w = $(window).width();  //ウィンドウ幅
+  h = $(window).height(); //ウィンドウ高さ
+  h1 = $("#top h1");
+  h1w = h1.width();
+  h1h = h1.height();
 
-	h1.css({'top': h/2-h1h, 'left': w/2-h1w/2});
-	$(window).resize(function(){
-		w = $(window).width();
-		h = $(window).height();
-		h1w = h1.width();
-		h1h = h1.height();
-		h1.css({'top': h/4-h1h - y/2.1, 'left': w/2-h1w/2});
-	});
+  h1.css({'top': h/2-h1h, 'left': w/2-h1w/2});
+  $(window).resize(function(){
+    w = $(window).width();
+    h = $(window).height();
+    h1w = h1.width();
+    h1h = h1.height();
+    h1.css({'top': h/4-h1h - y/2.1, 'left': w/2-h1w/2});
+  });
 
-	scroller();
-	$(window).on('scroll', scroller);
-	$('body').bind('touchmove', scroller);
+  scroller();
+  $(window).on('scroll', scroller);
+  $('body').bind('touchmove', scroller);
 
-	var flag = true;
+  var flag = true;
 
-	$("#main").on('scroll', function() {
-		innerY = document.getElementById("main").scrollTop;
-		if(innerY == 0) {
-			$("#main").css('overflow', 'hidden');
-			$("body").css('overflow', 'scroll');
-			flag = true
-		} else {
-			flag = false;
-		}
-	});
+  $("#main").on('scroll', function() {
+    innerY = document.getElementById("main").scrollTop;
+    if(innerY == 0) {
+      $("#main").css('overflow', 'hidden');
+      $("body").css('overflow', 'scroll');
+      flag = true
+    } else {
+      flag = false;
+    }
+  });
 
-	function scroller(){
-		y = $(window).scrollTop();
-		//$("#top").css('background-position', 'center ' + y/3);
-		h1.css('top', h/4-h1h - y/2.1);
-		if($('#main').height() == y && flag){
-			$("#main").css('overflow', 'scroll');
-			$("body").css('overflow', 'hidden');
-		}
-	}
+  function scroller(){
+    y = $(window).scrollTop();
+    //$("#top").css('background-position', 'center ' + y/3);
+    h1.css('top', h/4-h1h - y/2.1);
+    if($('#main').height() == y && flag){
+      $("#main").css('overflow', 'scroll');
+      $("body").css('overflow', 'hidden');
+    }
+  }
 });
