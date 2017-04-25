5b8a64ad6d9d9f1aaca38d4a1934b7ce46a63262
Shizukuishi Takuya <b1015208@fun.ac.jp>
2017-04-25 22:43:17 +0900

    [fix]fix scroll processing and styling

--- main.js
+++ main.js
@@ -3,48 +3,13 @@
  */
 
 $(function(){
-  w = $(window).width();  //ウィンドウ幅
-  h = $(window).height(); //ウィンドウ高さ
-  h1 = $("#top h1");
-  h1w = h1.width();
-  h1h = h1.height();
-
-  h1.css({'top': h/2-h1h, 'left': w/2-h1w/2});
-  $(window).resize(function(){
-    w = $(window).width();
-    h = $(window).height();
-    h1w = h1.width();
-    h1h = h1.height();
-    h1.css({'top': h/4-h1h - y/2.1, 'left': w/2-h1w/2});
-  });
-
-  scroller();
-  $(window).on('scroll', scroller);
-  $('body').bind('touchmove', scroller);
-
-  var flag = true;
-
-  $("#main").on('scroll', function() {
-    innerY = document.getElementById("main").scrollTop;
-    if(innerY == 0) {
-      $("#main").css('overflow', 'hidden');
-      $("body").css('overflow', 'scroll');
-      flag = true
-    } else {
-      flag = false;
-    }
-  });
-
-  function scroller(){
-    y = $(window).scrollTop();
-    h1.css('top', h/4-h1h - y/2.1);
-    if($('#main').height() <= y && flag){
-      $("#main").css('overflow', 'scroll');
-      $("body").css('overflow', 'hidden');
-    }
-  }
-
-
+  window.addEventListener("scroll", (() => {
+    let top_min_height = document.querySelector("#top h1").clientHeight;
+    return () => {
+      document.getElementById("top").style.height = Math.max(window.innerHeight - window.pageYOffset, top_min_height);
+    };
+  })());
+  
   //github-chart
   var img = $("#github-chart-img");
   img.load(function() {
